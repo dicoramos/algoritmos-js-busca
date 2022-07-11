9685b9d2ac97a2d5d7b279b13d11add40520a756
Ana <dicoramos@gmail.com>
2022-07-11 12:19:22 -0300

    Curso de Algoritmos com JavaScript II: MergeSort e quickSort

---
+++ aula-3-quickSort/encontraMenores.js
@@ -0,0 +1,45 @@
+const listaLivros = require('./array')
+
+function encontraMenores(pivo, array) {
+  let menores = 0;
+
+  for(let atual = 0; atual < array.length; atual++) {
+    let produtoAtual = array[atual]
+    if (produtoAtual.preco < pivo.preco) {
+      menores++
+    }
+  }
+
+  trocaLugar(array, array.indexOf(pivo), menores)//retorna o primeiro índice em que o elemento pode ser encontrado no array, retorna -1 caso o mesmo não esteja presente
+  return array
+}
+
+function trocaLugar(array, de, para) {
+  const elem1 = array[de]
+  const elem2 = array[para]
+
+  array[para] = elem1
+  array[de] = elem2
+}
+
+function divideNoPivo(array) {
+  let pivo = array[Math.floor(array.length / 2)];//retorna o menor número inteiro 
+
+   encontraMenores(pivo, array);
+  let valoresMenores = 0;
+
+  for(let analisando = 0; analisando < array.length; analisando++) {
+    let atual = array[analisando];
+    if(atual.preco <= pivo.preco && atual !== pivo) {
+      trocaLugar(array, analisando, valoresMenores)
+      valoresMenores++
+    }
+  }
+
+  return array;
+}
+
+console.log(divideNoPivo(listaLivros));
+console.log(encontraMenores(listaLivros[2], listaLivros));
+
+module.exports = trocaLugar;
